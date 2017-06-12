6fae78ee821048be617a21a2569443222d83ff92
vanesky <2538064643@qq.com>
2017-06-12 21:24:10 +0800

    data 4.7 banner重构

--- 组件示例/其他插件/ban/banner1/banner.js
+++ 组件示例/其他插件/ban/banner1/banner.js
@@ -1,7 +1,7 @@
 
 $.fn.banner = function(sel){
 
-    var that = $(this),time = '',dir = 'left',pause = false,isRun = false;
+    var that = $(this),time = '',pause = false,isRun = true;
 
     var banner = {};
 
@@ -32,12 +32,10 @@ $.fn.banner = function(sel){
         }
 
         this.slideRun = function(direction){
-            //如果正在运动则返回
-            if(isRun){return}
 
-            dir = direction;
+            if(!isRun){return}
 
-            var banner_ = direction == 'left' ? -banner_w:banner_w;
+            var banner_ = direction == 'left' ? banner_w:-banner_w;
 
             for(var i=0;i<listObj.length;i++){                       //判断当前是否为最后一个元素 如果是则把元素位置初始化
 
@@ -47,13 +45,8 @@ $.fn.banner = function(sel){
 
                     if(le==-banner_*(listObj.length-1)){
 
-                        listObj.eq(i).css('transition','');
-
                         listObj.eq(i).css('transform','translate('+banner_+'px)');
                     }
-
-
-
 
 
                 var leIng = listObj.eq(i).css('transform').split(',')[4];
@@ -71,51 +64,17 @@ $.fn.banner = function(sel){
                     });
             }
 
+            isRun = false;
+
             if(typeof sel.speed == 'undefined'|| typeof sel.mspeed =='undefined'){return false}
 
-            time = setTimeout(function(){banner.slideRun();},sel.speed);
+            if(pause){return false}
 
-            //设置正在运动
-            //isRun = true;
+            time = setTimeout(function(){banner.slideRun('left');},sel.speed);
+
+
         };
 
-        /*this.slideEventEnd = function(){
-
-            listObj.on('webkitTransitionEnd',function(){
-
-                listObj.css('transition','')
-
-                clearTimeout(time);
-
-                //运动完毕
-                isRun = false;
-
-                var banner_ = dir == 'left' ? banner_w:-banner_w;
-
-                for(var i=0;i<listObj.length;i++){    //判断当前是否为最后一个元素 如果是则把元素位置初始化
-
-                    var le = listObj.eq(i).css('transform').split(',')[4];
-
-                        le = Number(le.trim());
-
-                    if(le == -banner_*(listObj.length-1)){
-
-                        listObj.eq(i).css('transform','translateX('+banner_+'px)')
-                    }
-                }
-
-
-                //运动完成间隔
-                time = setTimeout(function(){
-                    //如果鼠标在点击区则取消
-                    if(pause){return}
-
-                    banner.slideRun('left');
-
-                },sel.speed);
-            })
-
-        };*/
 
         this.slideEvent = function(){
 
@@ -132,9 +91,9 @@ $.fn.banner = function(sel){
 
                 'mouseenter':function(){
 
-                    pause = true;
-
                     clearTimeout(time);
+
+                    pause = true;
                 },
 
                 'mouseleave':function(){
@@ -148,7 +107,15 @@ $.fn.banner = function(sel){
                     },sel.speed);
                 }
 
+            });
+
+            listObj.on('webkitTransitionEnd',function(){
+
+                listObj.css('transition','');
+
+                isRun = true;
             })
+
         };
 
         this.btList = function(index){
@@ -165,18 +132,17 @@ $.fn.banner = function(sel){
 
     banner.slideInit()
 
-    //banner.slideEventEnd()
 
     //动画完毕间隔执行
-    //banner.slideEvent();
+    banner.slideEvent();
 
-    //setTimeout(function(){
+    setTimeout(function(){
 
         banner.slideRun('left');
 
-    //},sel.speed)
+    },sel.speed)
 
-
+    $(window).resize(function(){banner_w = $(window).width();  banner.slideInit();})
 }
 
 

3a223a8c973c1ccd2d560ca882f16a88a90b6f96
vane <2538064643@qq.com>
2017-06-20 20:05:17 +0800

    component 2.2

--- 组件示例/原生组件/select/com-select.js
+++ 组件示例/原生组件/select/com-select.js
@@ -3,45 +3,62 @@ $.fn.comSelect = function(obj){
 
     var _this = $(this);
 
+    var headObj = _this.find('.sel-head');
+
+
+    //默认初始化所有列表选中第一项
     if(obj.init){
 
-        let headObj = _this.find('.sel-head');
+        headObj.val(_this.find('.sel-list').eq(0).text());
 
-        let contentObj = _this.find('.sel-content');
-
-        //默认选择第一个选项
-        headObj.text(_this.find('.sel-list').eq(0).text());
-
-        headObj.attr('data-val',_this.find('.sel-list').eq(0).attr('data-val'));
-
-        //绑定事件
-        _this.on('click','.sel-list',function(){
-
-            var dataVal = $(this).attr('data-val');
-
-            var text = $(this).text();
-
-            headObj.attr('data-val',dataVal).val(text);
-
-            contentObj.addClass('hide');
-
-        })
-
-        headObj.on({
-
-            'mouseenter':function(){
-
-                contentObj.removeClass('hide');
-            }
-
-        })
-
-        _this.mouseleave(function(){
-
-            contentObj.addClass('hide');
-        })
-
+        _this.find("input[name='selected']").val(_this.find('.sel-list').eq(0).attr('data-val'))
     }
 
-}
+    //绑定事件
+    _this.on('click','.sel-list',function(){
+
+        var that = $(this);
+
+        var text = that.text();
+
+        var dataVal = that.attr('data-val');
+
+            that.parents('.com-select').find('.sel-head').val(text);
+
+            dataVal ? that.parents('.com-select').find("input[name='selected']").val(dataVal) : '';
+
+    });
+
+    headObj.on({
+
+        'click':function(e){
+
+            e.stopPropagation();
+
+            var that = $(this);
+
+            var isTrue = that.parents('.com-select').find('.sel-content').hasClass('hide');
+
+            if(isTrue){
+
+                //关闭所有列表显示
+                _this.find('.sel-content').addClass('hide');
+
+                //切换当前列表显示
+                that.parents('.com-select').find('.sel-content').toggleClass('hide');
+            }else{
+
+                that.parents('.com-select').find('.sel-content').addClass('hide')
+            }
+        }
+
+    });
+
+    //点击外部关闭所有列表显示
+    $('body').on('click',function(){
+
+        _this.find('.sel-content').addClass('hide');
+    });
+
+};
 
--- 组件示例/原生组件/单选/com-radio.js
+++ 组件示例/原生组件/单选/com-radio.js
@@ -3,24 +3,23 @@ $.fn.comRadio = function(obj){
 
     var _this = $(this);
 
-    if(obj.init){
 
-        _this.find('input').click(function(ev){
+    _this.find('input').click(function(ev){
 
-            ev.stopPropagation();
+        ev.stopPropagation();
 
-        })
+    });
 
-        //绑定事件
-        _this.find('label').on('click',function(){
+    //绑定事件
+    _this.find('label').on('click',function(){
 
-            _this.find("[name='radioList']").removeClass('box-add');
+        //统一模块中同级样式消除
+        $(this).siblings('label').find("[data-name='radioList']").removeClass('box-add');
 
-            $(this).find("[name='radioList']").addClass('box-add');
+        $(this).find("[data-name='radioList']").addClass('box-add');
 
-        })
+    })
 
-    }
 
-}
+};
 

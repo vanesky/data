64a6289ff0f3184145a2eb49a19d2f128edd1bdd
vane <2538064643@qq.com>
2017-06-22 14:52:55 +0800

    component 2.2 check2.3

--- 组件示例/原生组件/多选/com-check.js
+++ 组件示例/原生组件/多选/com-check.js
@@ -7,21 +7,21 @@ $.fn.comCheck = function(obj){
     _this.find('input').change(function(ev){
 
         //当前点击checked对象
-        var input = $(this);
+        var that = $(this);
 
         /*
-         * @inputParent
+         * @parent
          * @当前点击模块最外层模块
          * @如果实例(_this)是多个，确定在当前模块实例中防止干扰其他实例。
          */
-        var inputParent = input.parents('.com-check');
+        var parent = that.parents('.com-check');
 
-        input.siblings("[data-name='checkList']").toggleClass('box-add');
+        that.siblings("[data-name='checkList']").toggleClass('box-add');
 
         var isTrue = false;
 
         //是否全选
-        inputParent.find(":checkbox").each(function(index,val){
+        parent.find(":checkbox").each(function(index,val){
 
             if(!val.checked){
 
@@ -36,42 +36,41 @@ $.fn.comCheck = function(obj){
 
         if(isTrue){
 
-            inputParent.find("[data-name='allCheck']").addClass('box-add');
+            parent.find("[data-name='allCheck']").addClass('box-add');
 
         }else{
 
-            inputParent.find("[data-name='allCheck']").removeClass('box-add');
+            parent.find("[data-name='allCheck']").removeClass('box-add');
         }
 
         ev.stopPropagation();
     });
 
-    //allCheck
+    //点击全部选中
     _this.find("label[data-name='all']").on('click',function(){
 
-        var label = $(this);
+        var that = $(this);
 
-        var has = label.find("[data-name='allCheck']").hasClass('box-add');
+        var parent = that.parents('.com-check');
+
+        var has = that.find("[data-name='allCheck']").hasClass('box-add');
 
         if(has){
 
             //删除选中
-            _this.find(":checkbox").prop('checked',false);
+            parent.find(":checkbox").prop('checked',false);
             //删除选中样式
-            _this.find("[data-name='checkList']").removeClass('box-add');
-            //删除全部样式
-            label.find("[data-name='allCheck']").removeClass('box-add')
+            parent.find("[data-name='checkList']").removeClass('box-add');
 
         }else{
 
             //选中
-            _this.find(":checkbox").prop('checked',true);
+            parent.find(":checkbox").prop('checked',true);
             //选中样式
-            _this.find("[data-name='checkList']").addClass('box-add');
-
-            //选中全部样式
-            label.find("[data-name='allCheck']").addClass('box-add');
+            parent.find("[data-name='checkList']").addClass('box-add');
         }
+
+        that.find("[data-name='allCheck']").toggleClass('box-add');
 
     })
 
